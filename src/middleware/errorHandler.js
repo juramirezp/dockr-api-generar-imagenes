87e4e34e5cb82766c0d25d0d83f6192905ca807e
Juan Ramirez <juramirezp@bch.bancodechile.cl>
2025-04-02 23:08:59 -0300

    fix errorHandler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,7 +1,7 @@
-module.exports = (err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).json({
-        message: 'Ha ocurrido un error en el servidor.',
-        error: process.env.NODE_ENV === 'production' ? {} : err.stack,
-    });
+export const errorHandler = (err, req, res, next) => {
+	console.error(err.stack);
+	res.status(500).json({
+		message: "Ha ocurrido un error en el servidor.",
+		error: process.env.NODE_ENV === "production" ? {} : err.stack,
+	});
 };
